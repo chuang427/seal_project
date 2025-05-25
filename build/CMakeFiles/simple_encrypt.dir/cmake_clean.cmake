f5494352004c3dc37cbcf91ac62ffce10c08cca1
chuang427 <213246928+chuang427@users.noreply.github.com>
2025-05-25 00:30:10 -0400

    First Homomorphic Encryption test using BFV [addition]

---
+++ build/CMakeFiles/simple_encrypt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple_encrypt.dir/simple_encrypt.cpp.o"
+  "CMakeFiles/simple_encrypt.dir/simple_encrypt.cpp.o.d"
+  "simple_encrypt"
+  "simple_encrypt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_encrypt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
